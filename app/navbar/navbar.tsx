df75c69d7d4d376cbfbcb7b4b45de10f303a7e5b
touchavin <touchavin@hotmail.com>
2024-04-18 12:36:42 +0700

    up

--- app/navbar/navbar.tsx
+++ app/navbar/navbar.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use client'
 import Link from "next/link";
 import { useState } from "react";
@@ -47,54 +46,5 @@ export default function Navbar(){
     
     
   );
-=======
-'use client'
-import Link from "next/link";
-import { useState } from "react";
-import { useSession } from "next-auth/react";
 
-export default function Navbar(){
-  const {data:session} = useSession();
-  const [searchQuery, setSearchQuery] = useState<string>("");
-  // console.log(session)
-  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchQuery(e.target.value);
-  };
-
-  return (
-    <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
-      <div className="container mx-auto flex justify-between items-center">
-        <div className="flex space-x-1">
-          {/* <input
-            type="text"
-            placeholder="ชื่ออาหาร"
-            className="border border-gray-300 px-2 py-1 rounded-lg focus:outline-none"
-            value={searchQuery}
-            onChange={handleSearchChange}
-          /> */}
-          {/* <div className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4" style={{ paddingTop: 5 }}>
-            <Link href={`/search/${searchQuery}`}>ค้นหา</Link>
-          </div> */}
-        </div>
-
-        <div>
-          <Link href="/" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-            หน้าแรก
-          </Link>
-
-          { !session && <Link href="/api/auth/signin" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-            เข้าสู่ระบบ
-          </Link>}
-          { session && <Link href="/api/auth/signout" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-            ออกจากระบบ
-          </Link>}
-          
-          
-        </div>
-      </div>
-    </nav>
-    
-    
-  );
->>>>>>> a8c45a5d7ac4667f726e8184c4e7feefb728aea0
 };
--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
  
@@ -16,23 +15,5 @@ export function middleware(request: NextRequest) {
 // See "Matching Paths" below to learn more
 export const config = {
   matcher: '/createmenu',
-=======
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
- 
-// This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-    const tokens=request.cookies.has("next-auth.session-token")
-    if (tokens) { 
-        return NextResponse.next();
-    } else {
-        return NextResponse.redirect(new URL('/api/auth/signin', request.url))
-    }
 
-}
- 
-// See "Matching Paths" below to learn more
-export const config = {
-  matcher: '/createmenu',
->>>>>>> a8c45a5d7ac4667f726e8184c4e7feefb728aea0
 } 
