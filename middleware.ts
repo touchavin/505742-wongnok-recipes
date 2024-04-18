f4c859a27b82fd9c440f2c6727559e63e969c007
touchavin <touchavin@hotmail.com>
2024-04-18 10:38:41 +0700

    login

---
+++ middleware.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+    const tokens=request.cookies.has("next-auth.session-token")
+    if (tokens) { 
+        return NextResponse.next();
+    } else {
+        return NextResponse.redirect(new URL('/api/auth/signin', request.url))
+    }
+
+}
+ 
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: '/createmenu',
+} 
